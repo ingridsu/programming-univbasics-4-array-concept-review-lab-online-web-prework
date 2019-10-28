592dd0cfcde8c8f741e936db45e8ee2b7f467923
Ingrid sura <sura.ingrid@gmail.com>
2019-10-28 17:43:57 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def find_element_index(array, value_to_find)
   counter=0 
   numbers= [2,4,5,6]
-  while numbers [counter]
+  while numbers [counter] do
+    puts number
 end
 
 def find_max_value(array)
